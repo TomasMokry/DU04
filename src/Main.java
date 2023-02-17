a6d7bb15d7f853bce0c325705f4f4df5b5908257
tomas_mokry <tomas.mokry@gmail.com>
2023-02-17 13:41:23 +0100

    Fizz Buzz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 1; i <= 100; i++){
+           if (i % 5 == 0 && i % 3 == 0){
+               System.out.println(i+" FizzBuzz");
+           } else if (i % 5 == 0) {
+               System.out.println(i+" Buzz");
+            } else if (i % 3 == 0) {
+               System.out.println(i+" Fizz");
+           } else {
+               System.out.println(i);
+           }
+        }
     }
 }
